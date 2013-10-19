ec2b830a25b204bbe7ef15bd9427c4fa6a4d934c
Charlie Root <shutingrz@gmail.com>
2013-10-19 23:53:45 +0900

    chaitee --rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vitocha::Application.config.secret_key_base = 'd498332cc332c88cb9d33836e205fdeb2c68e9e3e130e5aaaa957dbbbc6b4daaef3368f5cade8e9f5c20722a97cccb12d81c401b41897fd83d2bc3b8a4408606'
